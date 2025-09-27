eddb9ddac3ee426ff162129c8fc2449a0038f6a4
robertanchu <robertanchu@gmail.com>
2025-09-27 16:55:48 +0200

    u

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,25 @@
-export default { content:["./index.html","./src/**/*.{js,jsx}"], theme:{ extend:{ colors:{ brand:"#a22e24", dorado:"#d4af37" } } }, plugins:[] }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#d2232a",
+        dorado: "#d4af37"
+      },
+      container: { center: true, padding: "1rem" }
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1200px",
+        "2xl": "1200px"
+      }
+    }
+  },
+  plugins: [],
+}
