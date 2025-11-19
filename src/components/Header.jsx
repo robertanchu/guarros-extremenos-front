71941de6e5425fb3cc07c488d1cef40fc13fff5b
robertanchu <robertanchu@gmail.com>
2025-11-19 21:39:48 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function BrandLogo(){
         <img
           src="/logo/logo_horizontal_2.svg"
           alt="Guarros Extremeños"
-          className="w-auto"
+          className="w-auto h-auto"
           onError={() => setImgOk(false)}
           loading="eager"
           decoding="async"
